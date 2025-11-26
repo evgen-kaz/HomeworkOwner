426c201fd015ae15c83ea67d7e58cde568ddf506
evgen-kaz <141723080+evgen-kaz@users.noreply.github.com>
2025-11-26 15:16:48 +0800

    Update WebDriverConfig.java

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -2,11 +2,9 @@ package config;
 
 import org.aeonbits.owner.Config;
 
-@Config.LoadPolicy(Config.LoadType.MERGE) //при ней некорреткно работают 8 и 9 строчки кода. Нужно или или
+@Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "classpath:${env}.properties",
-       /* "classpath:local.properties", //из-за них не воспроизводятся тесты через раннер и терминал для локали
-        "classpath:remote.properties"*/
 })
 public interface WebDriverConfig extends Config {
 
@@ -15,7 +13,6 @@ public interface WebDriverConfig extends Config {
     String browser();
 
     @Key("browserVersion")
-    //@DefaultValue("") //локано сам поднимет, а для удаленной прописала в проперти
     String browserVersion();
 
     @Key("browserSize")
@@ -27,6 +24,5 @@ public interface WebDriverConfig extends Config {
     String baseUrl();
 
     @Key("remoteUrl")
-    //@DefaultValue("") //иначе запуск всегда будет удаленно происходить
     String remoteUrl();
 }
